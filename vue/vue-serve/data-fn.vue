9f7f38cc9e25535eab96d482e085f491b7b7a868
wangli08 <wangli08@finupgroup.com>
2019-10-30 11:37:59 +0800

    code

---
+++ vue/vue-serve/data-fn.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div>{{count}}</div>
+        <button @click="add">add</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'dataFn',
+    data: {
+        count: 0,
+    },
+    methods: {
+        add(){
+            console.log(this.count)
+            this.count += 1;
+        },
+    }
+}
+</script>
--- vue/vue-serve/test.vue
+++ vue/vue-serve/test.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
       <h1>hello vue serve</h1>
+      <DataFn></DataFn>
     </div>
 </template>
+
+<script>
+import DataFn from './data-fn'
+export default {
+  name: 'test',
+  components: {
+    DataFn
+  },
+}
+</script>
