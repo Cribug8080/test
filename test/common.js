5f6543a0598607dbfe6bff2dfe10d245d34997fc
rain <rain@infloww.com>
2024-12-13 17:49:07 +0800

    feat: component

---
+++ test/common.js
@@ -0,0 +1,18 @@
+
+// 加载公共 HTML 片段
+function loadHTML(elementId, filePath) {
+    return fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading HTML:', error));
+}
+
+// 在页面加载完成后调用
+document.addEventListener('DOMContentLoaded', () => {
+    loadHTML('header', './components/header.html').then(() => {
+        document.getElementsByClassName(location.pathname)?.[0]?.classList.add('selected')
+    });
+    loadHTML('footer', './components/footer.html');
+});
