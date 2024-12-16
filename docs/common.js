40668b31ba770c55d3e8481b1a207d93545d95a3
rain <rain@infloww.com>
2024-12-16 17:58:16 +0800

    feat: local css

--- docs/common.js
+++ docs/common.js
@@ -9,10 +9,17 @@ function loadHTML(elementId, filePath) {
         .catch(error => console.error('Error loading HTML:', error));
 }
 
+function addCss() {
+    document.head.innerHTML += `
+        <link rel="stylesheet" href="./index.css">
+    `;
+}
+
 // 在页面加载完成后调用
 document.addEventListener('DOMContentLoaded', () => {
     loadHTML('header', './components/header.html').then(() => {
         document.getElementsByClassName(location.pathname)?.[0]?.classList.add('selected')
     });
     loadHTML('footer', './components/footer.html');
+    addCss();
 });
